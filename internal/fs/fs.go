58a93e129f68228ba09b7305449dba741dac475d
Kevin Mulvey <kmulvey@linux.com>
2023-01-25 10:37:06 -0700

    remove old middleware and add AlreadyUpsized

--- internal/fs/fs.go
+++ internal/fs/fs.go
@@ -1,6 +1,7 @@
 package fs
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -8,7 +9,6 @@ import (
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/kmulvey/path"
-	log "github.com/sirupsen/logrus"
 )
 
 // ImageExtensionRegex are all the supported image extensions, and the only ones that will be included in file search/globbing.
@@ -31,49 +31,15 @@ func GetExistingFiles(dir string) ([]path.WatchEvent, error) {
 	return existingFiles, nil
 }
 
-// WatchEventToEntry watches the given directory for new files.
-func WatchEventToEntry(watchEvents chan path.WatchEvent) chan path.Entry {
-	var output = make(chan path.Entry, 1000)
+// AlreadyUpsized checks if we already upsized the image.
+func AlreadyUpsized(originalImage path.Entry, outputPath string) bool {
 
-	go func() {
-		for e := range watchEvents {
-			output <- e.Entry
-		}
-		close(output)
-	}()
+	var upsizedImagePath = filepath.Base(originalImage.AbsolutePath)
+	upsizedImagePath = filepath.Join(outputPath, strings.Replace(upsizedImagePath, filepath.Ext(upsizedImagePath), ".jpg", 1))
 
-	return output
-}
+	if _, err := os.Stat(upsizedImagePath); errors.Is(err, os.ErrNotExist) {
+		return false
+	}
 
-// DedupMiddleware is a channel middleware that attempts to remove files from the queue (channel) that have already been upsized.
-// This is a "best effort" function in that it is not perfect and deduplication should also be done at upsize time.
-// Example: img.jpg has already been upsized and is added to the channel, this func will remove it. However, this func only considers
-// images that have already been upsized so if img.jpg is added to the chan multiple times before it is upsized then all entries will
-// pass though this func. This func simply helps to
-func DedupMiddleware(outputPath string, input chan path.Entry) chan path.Entry {
-	var output = make(chan path.Entry, 1000)
-
-	go func() {
-		for originalImage := range input {
-			// image is the abs path
-			var upsizedImagePath = filepath.Base(originalImage.AbsolutePath)
-			upsizedImagePath = filepath.Join(outputPath, strings.Replace(upsizedImagePath, filepath.Ext(upsizedImagePath), ".jpg", 1))
-
-			if stat, _ := os.Stat(upsizedImagePath); stat != nil {
-				var err = os.Remove(originalImage.AbsolutePath)
-				if err != nil {
-					log.Errorf("error removing original file after upscale, err: %s", err.Error())
-				}
-				//log.WithFields(log.Fields{
-				//	"queue length": len(input),
-				//	"original":     originalImage.AbsolutePath,
-				//	"upsized":      upsizedImagePath,
-				//}).Info("already exists, skipping and deleting original")
-				continue
-			}
-			output <- originalImage
-		}
-		close(output)
-	}()
-	return output
+	return true
 }
