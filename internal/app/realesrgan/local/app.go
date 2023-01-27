632b4d98ab396ee59b3e939fc3b998be6746d9e0
Kevin Mulvey <kmulvey@linux.com>
2023-01-27 09:40:46 -0700

    start breaking out daemon

--- internal/app/realesrgan/local/app.go
+++ internal/app/realesrgan/local/app.go
@@ -7,6 +7,7 @@ import (
 	log "github.com/sirupsen/logrus"
 
 	"github.com/kmulvey/path"
+	"github.com/kmulvey/realesrgan-scheduler/internal/cache"
 	"github.com/kmulvey/realesrgan-scheduler/internal/fs"
 	"github.com/kmulvey/realesrgan-scheduler/internal/queue"
 	"github.com/prometheus/client_golang/prometheus"
@@ -14,13 +15,18 @@ import (
 
 type RealesrganLocal struct {
 	PromNamespace   string
+	RealesrganPath  string
+	OutputPath      string
+	NumGPUs         int
+	RemoveOriginals bool
 	UpsizeTimeGauge prometheus.Gauge
 	queue.Queue
+	cache.Cache
 }
 
 // NewRealesrganLocal is the constructor for running local upsizing. It takes a slice of existing files
 // and prepopulates the queue with them,  Run() takes a channel of watchEvents to stream files.
-func NewRealesrganLocal(promNamespace string, existingFiles []path.WatchEvent) (*RealesrganLocal, error) {
+func NewRealesrganLocal(promNamespace, cacheDir, realesrganPath, outputPath string, numGPUs int, removeOriginals bool) (*RealesrganLocal, error) {
 
 	var upsizeTime = prometheus.NewGauge(
 		prometheus.GaugeOpts{
@@ -31,23 +37,24 @@ func NewRealesrganLocal(promNamespace string, existingFiles []path.WatchEvent) (
 	)
 	prometheus.MustRegister(upsizeTime)
 
-	var rl = RealesrganLocal{PromNamespace: promNamespace, UpsizeTimeGauge: upsizeTime}
+	var rl = RealesrganLocal{PromNamespace: promNamespace, RealesrganPath: realesrganPath, OutputPath: outputPath, UpsizeTimeGauge: upsizeTime, NumGPUs: numGPUs, RemoveOriginals: removeOriginals}
 
 	rl.Queue = queue.NewQueue()
-	for _, existingFile := range existingFiles {
-		var err = rl.Queue.Add(existingFile.Entry)
-		if err != nil {
-			return nil, fmt.Errorf("problem adding existing files to queue: %w", err)
-		}
-	}
 
 	return &rl, nil
 }
 
 // Run starts an infinite loop that pulls files from the queue and upsizes them. This can be stopped by calling cancel() on the given context.
-func (rl *RealesrganLocal) Run(ctx context.Context, cmdPath, outputPath string, gpuID int, watchEvents chan path.WatchEvent, errors chan error) {
+func (rl *RealesrganLocal) Run(ctx context.Context, existingFiles []path.WatchEvent) {
 
-	go rl.UpsizeWorker(ctx, cmdPath, outputPath, gpuID, errors)
+	go rl.UpsizeWorker(ctx, rl.NumGPUs, errors)
+
+	for _, existingFile := range existingFiles {
+		var err = rl.Queue.Add(existingFile.Entry)
+		if err != nil {
+			errors <- fmt.Errorf("problem adding existing files to queue: %w", err)
+		}
+	}
 
 	for {
 		select {
@@ -62,6 +69,25 @@ func (rl *RealesrganLocal) Run(ctx context.Context, cmdPath, outputPath string, 
 
 		default:
 			log.Error(<-errors)
+		}
+	}
+}
+
+// Run starts an infinite loop that pulls files from the queue and upsizes them. This can be stopped by calling cancel() on the given context.
+func (rl *RealesrganLocal) Watch(ctx context.Context, watchEvents chan path.WatchEvent) {
+
+	go rl.UpsizeWorker(ctx, rl.NumGPUs)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+
+		case ev := <-watchEvents:
+			var err = rl.Queue.Add(ev.Entry)
+			if err != nil {
+				log.Errorf("error adding file to queue: %s", err)
+			}
 		}
 	}
 }
