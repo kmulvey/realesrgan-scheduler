b07776e99c51753f1d3c374a2336109c4bcfc1ed
Kevin Mulvey <kmulvey@linux.com>
2023-01-28 15:21:43 -0700

    real asserts in TestPrettyPrintFileSizes

--- internal/app/realesrgan/local/worker_test.go
+++ internal/app/realesrgan/local/worker_test.go
@@ -1,18 +1,21 @@
 package local
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPrettyPrintFileSizes(t *testing.T) {
-	fmt.Println(prettyPrintFileSizes(100))
-	fmt.Println(prettyPrintFileSizes(1000))
-	fmt.Println(prettyPrintFileSizes(10000))
-	fmt.Println(prettyPrintFileSizes(100000))
-	fmt.Println(prettyPrintFileSizes(1000000))
-	fmt.Println(prettyPrintFileSizes(10000000))
-	fmt.Println(prettyPrintFileSizes(100000000))
-	fmt.Println(prettyPrintFileSizes(987000000))
-	fmt.Println(prettyPrintFileSizes(1000000000))
+	t.Parallel()
+
+	assert.Equal(t, "100 bytes", prettyPrintFileSizes(100))
+	assert.Equal(t, "1 kb", prettyPrintFileSizes(1000))
+	assert.Equal(t, "10 kb", prettyPrintFileSizes(10000))
+	assert.Equal(t, "100 kb", prettyPrintFileSizes(100000))
+	assert.Equal(t, "1 mb", prettyPrintFileSizes(1000000))
+	assert.Equal(t, "10 mb", prettyPrintFileSizes(10000000))
+	assert.Equal(t, "100 mb", prettyPrintFileSizes(100000000))
+	assert.Equal(t, "987 mb", prettyPrintFileSizes(987000000))
+	assert.Equal(t, "1 gb", prettyPrintFileSizes(1000000000))
 }
