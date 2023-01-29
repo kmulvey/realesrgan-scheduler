4a475ed3695f17b97d183867b1f94e0d52d4ba38
Kevin Mulvey <kmulvey@linux.com>
2023-01-29 10:50:58 -0700

    manage cache cli

---
+++ cmd/managecache/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"sync"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/kmulvey/path"
+	"github.com/kmulvey/realesrgan-scheduler/internal/cache"
+	"go.szostok.io/version"
+	"go.szostok.io/version/printer"
+)
+
+func main() {
+	// get the user options
+	var cacheDir path.Path
+	var searchTerm string
+	var h, ver bool
+
+	flag.Var(&cacheDir, "cache-dir", "where to store the cache file for failed upsizes")
+	flag.StringVar(&searchTerm, "search", "", "search")
+	flag.BoolVar(&ver, "version", false, "print version")
+	flag.BoolVar(&h, "help", false, "print options")
+	flag.Parse()
+
+	if h {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	if ver {
+		var verPrinter = printer.New()
+		var info = version.Get()
+		if err := verPrinter.PrintInfo(os.Stdout, info); err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	}
+
+	log.Infof("Config: cacheDir: %s, searchTerm: %s", cacheDir.ComputedPath.AbsolutePath, searchTerm)
+
+	var db, err = cache.New(cacheDir.ComputedPath.AbsolutePath)
+	if err != nil {
+		log.Errorf("error opening badger dir: %s", err)
+	}
+
+	var images = make(chan string)
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		for img := range images {
+			fmt.Println(img)
+		}
+		wg.Done()
+	}()
+
+	err = db.ListKeys(searchTerm, images)
+	if err != nil {
+		log.Errorf("error listing keys: %s", err)
+	}
+
+	wg.Wait()
+}
