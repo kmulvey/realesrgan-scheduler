4a37ecd547fd87ae06b3f6a8dac6ff325318f7f4
Kevin Mulvey <kmulvey@linux.com>
2023-01-29 09:57:19 -0700

    get local working

--- cmd/local/main.go
+++ cmd/local/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"context"
-	"errors"
 	"flag"
-	"fmt"
 	"net/http"
 	"os"
 	"regexp"
@@ -12,6 +10,8 @@ import (
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/kmulvey/path"
+	"github.com/kmulvey/realesrgan-scheduler/internal/app/realesrgan/local"
+	"github.com/kmulvey/realesrgan-scheduler/internal/fs"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	log "github.com/sirupsen/logrus"
 	"go.szostok.io/version"
@@ -41,29 +41,17 @@ func main() {
 	}()
 
 	// get the user options
-	var inputImages path.Path
-	var upscaledImages path.Path
+	var originalImages, upscaledImages, cacheDir path.Path
 	var realesrganPath string
-	var username string
-	var password string
-	var http bool
-	var threads int
-	var port int
-	var daemon bool
-	var v bool
-	var h bool
+	var daemon, removeOriginals, h, ver bool
 
-	flag.Var(&inputImages, "uploaded-images-dir", "where to store the uploaded images")
+	flag.Var(&originalImages, "original-images-dir", "path to the original (input) images")
 	flag.Var(&upscaledImages, "upscaled-images-dir", "where to store the upscaled images")
+	flag.Var(&cacheDir, "cache-dir", "where to store the cache file for failed upsizes")
 	flag.StringVar(&realesrganPath, "realesrgan-path", "realesrgan-ncnn-vulkan", "where the realesrgan binary is")
-	flag.StringVar(&username, "username", "", "username for the webserver")
-	flag.StringVar(&password, "password", "", "password for the webserver")
-	flag.BoolVar(&http, "http", false, "true to run in webserver mode")
-	flag.IntVar(&threads, "threads", 1, "number of gpus")
-	flag.IntVar(&port, "port", 3000, "port number for the webserver")
+	flag.BoolVar(&removeOriginals, "remove-originals", false, "delete original images after upsizing")
 	flag.BoolVar(&daemon, "d", false, "run as a daemon (does not quit)")
-	flag.BoolVar(&v, "version", false, "print version")
-	flag.BoolVar(&v, "v", false, "print version")
+	flag.BoolVar(&ver, "version", false, "print version")
 	flag.BoolVar(&h, "help", false, "print options")
 	flag.Parse()
 
@@ -72,7 +60,7 @@ func main() {
 		os.Exit(0)
 	}
 
-	if v {
+	if ver {
 		var verPrinter = printer.New()
 		var info = version.Get()
 		if err := verPrinter.PrintInfo(os.Stdout, info); err != nil {
@@ -81,50 +69,47 @@ func main() {
 		os.Exit(0)
 	}
 
-	if err := mkdir(inputImages.ComputedPath.AbsolutePath); err != nil {
-		log.Fatalf("error creating upload dir: %s", err.Error())
+	log.Infof("Config: originalImages: %s, upscaledImages: %s, realesrganPath: %s, cacheDir: %s, removeOriginals: %t, daemon: %t",
+		originalImages.ComputedPath.AbsolutePath,
+		upscaledImages.ComputedPath.AbsolutePath,
+		realesrganPath,
+		cacheDir.ComputedPath.AbsolutePath,
+		removeOriginals,
+		daemon)
+
+	var images, err = path.List(originalImages.ComputedPath.AbsolutePath, path.NewRegexListFilter(fs.ImageExtensionRegex))
+	if err != nil {
+		log.Fatalf("error getting existing upsized dirs: %s", err)
 	}
 
-	if err := mkdir(upscaledImages.ComputedPath.AbsolutePath); err != nil {
-		log.Fatalf("error creating upscale dir: %s", err.Error())
+	rl, err := local.NewRealesrganLocal(promNamespace, cacheDir.ComputedPath.AbsolutePath, realesrganPath, upscaledImages.ComputedPath.AbsolutePath, 1, removeOriginals)
+	if err != nil {
+		log.Fatalf("error in: NewRealesrganLocal %s", err)
 	}
 
-	var originalImages = make(chan path.WatchEvent)
-	var entries = watchEventToEntry(originalImages)
-	var dedupedImages = dedupMiddleware(upscaledImages.ComputedPath.AbsolutePath, entries)
-	var upsizedImages = make(chan path.Entry)
-
-	go runWorkers(realesrganPath, upscaledImages.ComputedPath.AbsolutePath, 0, dedupedImages, upsizedImages)
-
-	// we dont need upsizedImages in this mode but we still need to drain the chan
-	go func() {
-		for range upsizedImages {
+	// load up existing images
+	for _, image := range images {
+		err = rl.AddImage(image)
+		if err != nil {
+			log.Fatalf("error adding image to queue: %s", err)
 		}
-	}()
-
-	fmt.Println("before getExistingFiles")
-	if err := getExistingFiles(inputImages.ComputedPath.AbsolutePath, originalImages); err != nil {
-		log.Fatalf("error in :getExistingFiles %s", err)
 	}
-
-	log.WithFields(log.Fields{
-		"watching directory": inputImages.ComputedPath.AbsolutePath,
-	}).Info("started")
 
 	if daemon {
-		if err := path.WatchDir(ctx, inputImages.ComputedPath.AbsolutePath, originalImages, path.NewOpWatchFilter(fsnotify.Create), path.NewRegexWatchFilter(regexp.MustCompile(".*.jpg$|.*.jpeg$|.*.png$|.*.webp$"))); err != nil {
+
+		var watchEvents = make(chan path.WatchEvent)
+		rl.Watch(watchEvents)
+
+		if err := path.WatchDir(ctx, originalImages.ComputedPath.AbsolutePath, watchEvents, path.NewOpWatchFilter(fsnotify.Create), path.NewRegexWatchFilter(regexp.MustCompile(".*.jpg$|.*.jpeg$|.*.png$|.*.webp$"))); err != nil {
 			log.Fatalf("error in watchDir: %s", err)
 		}
-	}
-	cancel()
-}
+	} else {
 
-func mkdir(path string) error {
-	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
-		err := os.Mkdir(path, os.ModePerm)
+		err = rl.Run(nil) // images were already added above
 		if err != nil {
-			return fmt.Errorf("error creating dir: %s, err: %w", path, err)
+			log.Errorf("error in Run(): %s", err)
 		}
 	}
-	return nil
+
+	cancel()
 }
