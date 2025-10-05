eedca19138ffb3252e1ab7eebd7ee6299e366e62
Kevin Mulvey <kmulvey@linux.com>
2025-10-04 19:00:53 -0600

    mock images and binary

--- testimages/images.go
+++ testimages/images.go
@@ -3,10 +3,13 @@ package testimages
 import _ "embed"
 
 //go:embed fox.jpg
-var FoxJPG []byte // Embed the .jpg file as a byte slice
+var FoxJPG []byte
 
 //go:embed fox.png
-var FoxPNG []byte // Embed the .jpg file as a byte slice
+var FoxPNG []byte
 
 //go:embed fox.avif
-var FoxAVIF []byte // Embed the .jpg file as a byte slice
+var FoxAVIF []byte
+
+//go:embed not_an_image.jpg
+var NotAnImage []byte
